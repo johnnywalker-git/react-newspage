50f7e6ac7e5b6b2d0b658d67fee3668bd1649bb1
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-01-26 23:01:41 +0000

    Setting up project.

--- react-newspage/src/App.jsx
+++ react-newspage/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const apiKey = "426a94f11afe46d0b56ad60680b44025"
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+  export default function YourComponent() {
+
+    const fetchNews = async (apiKey) => {
+      const response = await fetch(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${apiKey}`)
+      const data = await response.json()
+      console.log(data)
+    }
+
+    return (
+      <main>
+  <div className="mainComponent">
+    <p>{data}</p>
+  </div>
+  </main>
   )
-}
+  }
 
-export default App
+
+
