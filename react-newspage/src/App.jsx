a4bb51270fc5ca5e52f02f41d3f999ef3e6b9c47
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-01-29 16:10:28 +0000

    Added Basic interface

--- react-newspage/src/App.jsx
+++ react-newspage/src/App.jsx
@@ -3,24 +3,17 @@ import ReactDOM from 'react-dom'
 import { useState, useEffect } from 'react'
 import './App.css'
 
-const apiKey = "426a94f11afe46d0b56ad60680b44025"
 
 
-  export default function YourComponent() {
-
-    const fetchNews = async (apiKey) => {
-      const response = await fetch(`https://newsapi.org/v2/top-headlines?country=gb&apiKey=${apiKey}`)
-      const data = await response.json()
-      console.log(data)
-    }
+  export default function NewsStory(props) {
 
     return (
-      <main>
-  <div className="mainComponent">
-    <p>{data}</p>
-  </div>
-  </main>
-  )
+      <div className="newsContainerBackground">
+
+        <h2>Hello</h2>
+        <p>Hello again</p>
+      </div>
+    )
   }
 
 
--- react-newspage/src/main.jsx
+++ react-newspage/src/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import YourComponent from './App'
-
+import NewsStory from './App'
 import '../style.css'
+
+const apiKey = "426a94f11afe46d0b56ad60680b44025"
+
+var url = 'https://newsapi.org/v2/everything?' +
+          'q=Apple&' +
+          'from=2023-01-29&' +
+          'sortBy=popularity&' +
+          'apiKey=426a94f11afe46d0b56ad60680b44025';
+
+var req = new Request(url);
+
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <YourComponent />
+    <NewsStory />
 
 )
